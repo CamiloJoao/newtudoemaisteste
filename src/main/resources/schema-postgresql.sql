e28e532b1688a0619927b837dfe52a812570a051
CamiloJoao <142325482+CamiloJoao@users.noreply.github.com>
2025-06-18 14:40:13 -0300

    commit

--- src/main/resources/schema-postgresql.sql
+++ src/main/resources/schema-postgresql.sql
@@ -1,9 +1,6 @@
-CREATE TABLE IF NOT EXISTS users (
-  user_id SERIAL PRIMARY KEY,
-  username VARCHAR(45) NOT NULL,
-  password VARCHAR(64) NOT NULL,
-  role VARCHAR(45) NOT NULL,
-  enabled BOOLEAN DEFAULT TRUE
+CREATE TABLE IF NOT EXISTS categoria (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(20)
 );
 
 CREATE TABLE IF NOT EXISTS produto (
@@ -15,19 +12,22 @@ CREATE TABLE IF NOT EXISTS produto (
     CONSTRAINT fk_categoria FOREIGN KEY (categoria) REFERENCES categoria(id)
 );
 
-CREATE TABLE IF NOT EXISTS categoria (
-    id SERIAL PRIMARY KEY,
-    nome VARCHAR(20)
+CREATE TABLE IF NOT EXISTS users (
+  user_id SERIAL PRIMARY KEY,
+  username VARCHAR(45) NOT NULL UNIQUE,
+  password VARCHAR(64) NOT NULL,
+  role VARCHAR(45) NOT NULL,
+  enabled BOOLEAN DEFAULT TRUE
 );
-
-
 
 INSERT INTO users (username, password, role, enabled)
 VALUES ('user',
 '$2a$12$vLYbNHhMjCaffmemGzh2au.TAtMATl24Y9do5fqDxNPJ9Xvm4kLQy',
-'ROLE_USER', TRUE);
- 
+'ROLE_USER', TRUE)
+ON CONFLICT (username) DO NOTHING;
+
 INSERT INTO users (username, password, role, enabled)
 VALUES ('admin',
 '$2a$12$yXWeF5BmghdWH25UPCyNR.vGKhqfOHHjCxa4qHJdvCN.AzVdSH9vG',
-'ROLE_ADMIN', TRUE);
+'ROLE_ADMIN', TRUE)
+ON CONFLICT (username) DO NOTHING;
